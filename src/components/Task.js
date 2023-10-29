0d5d90b07b9181310f0eab60444d77a7223126c8
clairemuiru1 <muiruclaire@gmail.com>
2023-10-29 20:09:51 +0300

    "Commit changes"

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,15 @@
 import React from "react";
 
 function Task() {
+
+  function handleDelete() {
+    Task.filter
+  }
   return (
     <div className="task">
       <div className="label">CATEGORY HERE</div>
       <div className="text">TEXT HERE</div>
-      <button className="delete">X</button>
+      <button className="delete" onClick={handleDelete}>X</button>
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList(props) {
   return (
     <div className="tasks">
       {/* display a list of tasks using Task component */}
+      <Task/>
     </div>
   );
 }
