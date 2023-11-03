ae4d4082825f21592fa2d9dc3b264849023aa4ed
clairemuiru1 <muiruclaire@gmail.com>
2023-11-03 16:43:14 +0300

    "commit changes"

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,12 +1,12 @@
 import React from "react";
-import Task from "./Task";
+import Task from "./Task"; 
 
-function TaskList(props) {
+function TaskList({ tasks, onDeleteTask }) {
   return (
     <div className="tasks">
-      {props.task.map((task)=>
-      <Task  key={task.id}task={task}/>)}
-      
+      {tasks.map((task) => (
+        <Task key={task.id} task={task} onDeleteTask={onDeleteTask} />
+      ))}
     </div>
   );
 }
